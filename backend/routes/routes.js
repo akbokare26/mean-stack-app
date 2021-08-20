bfb61be4033d51889e836fc67ebd27c898c97b75
Aniket <aniket.21810637@viit.ac.in>
2021-08-20 10:48:52 +0530

    Initial commit

---
+++ backend/routes/routes.js
@@ -0,0 +1,112 @@
+const express = require('express');
+const router = express.Router();
+const Employee = require('../models/employee.js');
+const ObjectId = require('mongoose').Types.ObjectId;
+
+
+
+
+// get all data
+router.get('/getallemployee',(req,res)=>{
+        Employee.find({},(err, doc)=>{
+            if(err){
+                console.log('Error in Get Data' +err)
+            }
+            else{
+                res.send(doc);
+            }
+
+        });
+});
+
+
+//get by id
+router.get('/:id',(req,res)=>{
+
+    if(ObjectId.isValid(req.params.id)){
+        Employee.findById(req.params.id,(err, doc)=>{
+            if(err){
+                console.log('Error in Get Employee by id' +err)
+            }
+            else{
+                res.send(doc);
+            }
+
+        });
+
+    }
+    else{
+        return res.status(400).send('No record found with id' + req.params.id)
+    }
+
+});
+
+
+//Post
+router.post('/employee',(req,res)=>{
+    let emp = new Employee({
+    name :  req.body.name,
+    position:  req.body.position,
+    dept: req.body.dept
+    });
+
+    emp.save((err, doc)=>{
+        if(err){
+            console.log('Error in post data' + err)
+        }
+        else{
+            res.send(doc);
+        }
+    })
+
+});
+
+// delete API
+router.delete('/:id',(req,res)=>{
+    if(ObjectId.isValid(req.params.id)){
+        Employee.findByIdAndRemove(req.params.id,(err, doc)=>{
+            if(err){
+                console.log('Error in Delete Employee by id' +err)
+            }
+            else{
+                res.send(doc);
+            }
+
+        });
+
+    }
+    else{
+        return res.status(400).send('No record found with ' + req.params.id)
+    }
+
+});
+
+
+// Put
+router.put('/:id',(req,res)=>{
+
+    let emp = {
+        name :  req.body.name,
+        position:  req.body.position,
+        dept: req.body.dept
+        };
+
+    if(ObjectId.isValid(req.params.id)){
+        Employee.findByIdAndUpdate(req.params.id,{$set:emp},{new:true},(err, doc)=>{
+            if(err){
+                console.log('Error in Update Employee by id' +err)
+            }
+            else{
+                res.send(doc);
+            }
+
+        });
+
+    }
+    else{
+        return res.status(400).send('No record found with id' + req.params.id)
+    }
+
+});
+
+module.exports = router;
---
+++ frontend/src/app/appServices/employee.service.ts
@@ -0,0 +1,33 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Employee } from '../appModels/employee.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class EmployeeService {
+
+  url = 'http://localhost:3000/employees';
+
+  constructor(private http : HttpClient) { }
+
+  addEmployee(emp:Employee){
+    return this.http.post(`${this.url}/employee`,emp);
+  }
+
+  getEmployeeList() {
+    return this.http.get(`${this.url}/getallemployee`);
+  }
+
+  getEmployeebyId(id: string) {
+    return this.http.get(this.url+`/${id}`);
+  }
+
+  updateEmployee(emp:Employee){
+    return this.http.put(`${this.url}/${emp._id}`, emp);
+  }
+
+  deleteEmployee(id: any){
+    return this.http.delete(`${this.url}/${id}`);
+  }
+}
